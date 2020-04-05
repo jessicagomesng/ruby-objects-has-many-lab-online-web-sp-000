9ad57cd92b11df92e881ff730eeaae6e220bd75c
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-05 13:16:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,15 @@
 class Artist
-  attr_accessor :name 
+  attr_accessor :name
 
   def initialize(name)
     @name = name
   end
+
+  def songs
+  end
+
+  def add_song(song)
+    song.artist = self
+  end 
 
 end
